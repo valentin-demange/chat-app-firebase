bda50c534ffa258992a01cde5f8a2f1f1c12f19e
valentin-demange <demange.valentin@gmail.com>
2022-10-10 17:40:29 +0200

    Patch for Gilbert's name to display correctly

--- components/Login/signInGoogle.jsx
+++ components/Login/signInGoogle.jsx
@@ -32,7 +32,13 @@ export default function SignInGoogle() {
       const userRef = doc(db, 'users', currentUser.user.uid);
       console.log("updating currentUser info in database")
       await setDoc(userRef, {
-        name: currentUser.user.displayName,
+        // For displayName, we consider "tokenResponse" and not "user" as a fix for Gilbert (details below)
+        // After I changed my account name from "Valentin Demange" to "Gilbert", currentUser.user.displayName
+        // was still "Valentin Demange". I had to change it to currentUser._tokenResponse.displayName to have 
+        // Gilbert displayed. Hope this workaround has not side-effects.
+        // Then I tried to switch to _tokenResponse for all fields, but had issues with photoUrl then, so I 
+        // kept that mixed state
+        name: currentUser._tokenResponse.displayName,
         photoURL: currentUser.user.photoURL,
         uid: currentUser.user.uid,
       });
