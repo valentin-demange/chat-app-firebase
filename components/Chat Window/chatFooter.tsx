aa3e14b2476b816f0211a82d38754f755acd6b74
valentin-demange <demange.valentin@gmail.com>
2022-10-11 22:15:52 +0200

    Start Gilbert with OpenAi implementation

--- components/Chat Window/chatFooter.tsx
+++ components/Chat Window/chatFooter.tsx
@@ -2,11 +2,11 @@ import styles from "./styles.module.css";
 import { Box, IconButton, Input } from "@chakra-ui/react";
 import { ArrowRightIcon } from "@chakra-ui/icons";
 import React, { useState } from "react";
-import { collection, addDoc, setDoc, updateDoc, doc } from "firebase/firestore";
+import { collection, addDoc, getDoc, updateDoc, doc } from "firebase/firestore";
 import { db } from "utils/firebase";
-import firebase from 'firebase/compat/app'
-import { useContext } from 'react';
-import { CurrentUserContext, CurrentChatContext } from 'utils/context';
+import firebase from "firebase/compat/app";
+import { useContext } from "react";
+import { CurrentUserContext, CurrentChatContext } from "utils/context";
 
 export default function ChatFooter() {
   const [textMessage, setTextMessage] = useState("");
@@ -15,50 +15,80 @@ export default function ChatFooter() {
   const msgCollectionPath = [chatPath, "messages"].join("/");
   const currentUser = useContext(CurrentUserContext);
 
-  const handleInputChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+  const handleInputChange = (e: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
     setTextMessage(e.target.value);
   };
 
-  const handleOnSubmit = async (e: { preventDefault: () => void; }) => {
+  const handleOnSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
-    if (textMessage === "") return
+    if (textMessage === "") return;
     setTextMessage("");
+
     // Add a new document with a generated id.
     const timestamp = firebase.firestore.FieldValue.serverTimestamp();
-    await updateDoc(doc(db, "chats", currentChat), {
-      lastMessage: timestamp,
-    });
-    const docRef = await addDoc(collection(db, msgCollectionPath), {
+    const docRef = doc(db, "chats", currentChat);
+    const docSnap = await getDoc(docRef).then(() =>
+      updateDoc(docRef, {
+        lastMessage: timestamp,
+      })
+    )//.then(() => handleGilbert());
+    await addDoc(collection(db, msgCollectionPath), {
       profilePicUrl: currentUser.photoURL,
       authorId: currentUser.uid,
       text: textMessage,
       timestamp: timestamp,
-    }); 
-    // debugger
-    console.log("Document written with ID: ", docRef.id);
+    });
+
+    // const handleGilbert = async () => {
+    //   debugger
+
+    //   if (docSnap.exists()) {
+    //     const chatInfo = docSnap.data();
+    //     const isGilbert =
+    //       chatInfo.membersUid.filter(
+    //         (uid: string) => currentUser.uid !== uid
+    //       )[0] === "Gilbert";
+    //     if (isGilbert) await chatWithOpenAi();
+    //   } else {
+    //     // doc.data() will be undefined in this case
+    //     console.log("No such document!");
+    //   }
+
+    //   const chatWithOpenAi = async () => {
+    //     // const isFirstMsg = chatInfo.lastTimest
+    //     debugger;
+    //     // chatInfo.hasOwnProperty('lastMessage')
+    //   };
+  
+    // };
+
+    // // Gilbert scenario
+    // await handleGilbert();
+  
   };
+
 
   return (
     <form onSubmit={handleOnSubmit}>
-
-    <Box borderColor="gray.400" className={styles.chatFooter}>
-
-      <Input
-        placeholder="Type your message.."
-        flex={1}
-        onChange={handleInputChange}
-        value={textMessage}
-      />
-      <IconButton
-        variant="ghost"
-        aria-label="Add Chat"
-        isRound={true}
-        icon={<ArrowRightIcon />}
-        size="md"
-        fontSize={14}
-        type="submit"
-      />
-    </Box>
+      <Box borderColor="gray.400" className={styles.chatFooter}>
+        <Input
+          placeholder="Type your message.."
+          flex={1}
+          onChange={handleInputChange}
+          value={textMessage}
+        />
+        <IconButton
+          variant="ghost"
+          aria-label="Add Chat"
+          isRound={true}
+          icon={<ArrowRightIcon />}
+          size="md"
+          fontSize={14}
+          type="submit"
+        />
+      </Box>
     </form>
   );
 }
--- components/Login/signInGoogle.tsx
+++ components/Login/signInGoogle.tsx
@@ -31,9 +31,9 @@ export default function SignInGoogle() {
       // debugger
       const userRef = doc(db, 'users', currentUser.user.uid);
       console.log("updating currentUser info in database")
-      // If user = Gilbert, we do not overwrite info, since it will change
-      // username back to "Valentin Demange" :(
-      if (currentUser.user.uid === "s8EZPnz4RgTRxXaEUTOiQSwDxIC3") return
+      // // If user = Gilbert, we do not overwrite info, since it will change
+      // // username back to "Valentin Demange" :(
+      // if (currentUser.user.uid === "s8EZPnz4RgTRxXaEUTOiQSwDxIC3") return
       await setDoc(userRef, {
         name: currentUser.user.displayName,
         photoURL: currentUser.user.photoURL,
